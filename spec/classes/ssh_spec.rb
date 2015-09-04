faa599b42634a7f87d96b359b6e9cbb86926e7e7
Chris Belyea <cbelyea@singlestoneconsulting.com>
2015-09-04 08:08:27 -0400

    Run retrospec

---
+++ spec/classes/ssh_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+require 'shared_contexts'
+
+describe 'ssh' do
+  # by default the hiera integration uses hiera data from the shared_contexts.rb file
+  # but basically to mock hiera you first need to add a key/value pair
+  # to the specific context in the spec/shared_contexts.rb file
+  # Note: you can only use a single hiera context per describe/context block
+  # rspec-puppet does not allow you to swap out hiera data on a per test block
+  #include_context :hiera
+
+
+  # below is the facts hash that gives you the ability to mock
+  # facts on a per describe/context block.  If you use a fact in your
+  # manifest you should mock the facts below.
+  let(:facts) do
+    {}
+  end
+  # below is a list of the resource parameters that you can override.
+  # By default all non-required parameters are commented out,
+  # while all required parameters will require you to add a value
+  let(:params) do
+    {
+      #:allow_groups => ,
+      #:allow_tcp_forwarding => no,
+      #:allow_users => ,
+      #:ciphers => aes128-ctr,aes192-ctr,aes256-ctr,
+      #:client_alive_count_max => 0,
+      #:client_alive_interval => 300,
+      #:deny_groups => ,
+      #:deny_users => ,
+      #:hostbased_authentication => no,
+      #:ignore_rhosts => yes,
+      #:log_level => VERBOSE,
+      #:macs => hmac-sha1,
+      #:max_auth_tries => 4,
+      #:package_ensure => present,
+      #:package_name => openssh-server,
+      #:permit_empty_passwords => no,
+      #:permit_root_login => no,
+      #:permit_user_environment => no,
+      #:port => 22,
+      #:protocol => 2,
+      #:rhosts_rsa_authentication => no,
+      #:service_enable => true,
+      #:service_ensure => running,
+      #:ssh_banner => /etc/motd,
+      #:sshd_config_template => ssh/sshd_config.erb,
+      #:x11_forwarding => no,
+    }
+  end
+  # add these two lines in a single test block to enable puppet and hiera debug mode
+  # Puppet::Util::Log.level = :debug
+  # Puppet::Util::Log.newdestination(:console)
+end
---
+++ spec/shared_contexts.rb
@@ -0,0 +1,69 @@
+# optional, this should be the path to where the hiera data config file is in this repo
+# You must update this if your actual hiera data lives inside your module.
+# I only assume you have a separate repository for hieradata and its include in your .fixtures
+hiera_config_file = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures','modules','hieradata', 'hiera.yaml'))
+
+# hiera_config and hiera_data are mutually exclusive contexts.
+
+shared_content :global_hiera_data do
+  let(:hiera_data) do
+     {
+       #"ssh::allow_groups" => '',
+       #"ssh::allow_tcp_forwarding" => '',
+       #"ssh::allow_users" => '',
+       #"ssh::ciphers" => '',
+       #"ssh::client_alive_count_max" => '',
+       #"ssh::client_alive_interval" => '',
+       #"ssh::deny_groups" => '',
+       #"ssh::deny_users" => '',
+       #"ssh::hostbased_authentication" => '',
+       #"ssh::ignore_rhosts" => '',
+       #"ssh::log_level" => '',
+       #"ssh::macs" => '',
+       #"ssh::max_auth_tries" => '',
+       #"ssh::package_ensure" => '',
+       #"ssh::package_name" => '',
+       #"ssh::permit_empty_passwords" => '',
+       #"ssh::permit_root_login" => '',
+       #"ssh::permit_user_environment" => '',
+       #"ssh::port" => '',
+       #"ssh::protocol" => '',
+       #"ssh::rhosts_rsa_authentication" => '',
+       #"ssh::service_enable" => '',
+       #"ssh::service_ensure" => '',
+       #"ssh::ssh_banner" => '',
+       #"ssh::sshd_config_template" => '',
+       #"ssh::x11_forwarding" => '',
+     
+     }
+  end
+end
+
+shared_context :hiera do
+    # example only,
+    let(:hiera_data) do
+        {:some_key => "some_value" }
+    end
+end
+
+shared_context :linux_hiera do
+    # example only,
+    let(:hiera_data) do
+        {:some_key => "some_value" }
+    end
+end
+
+# In case you want a more specific set of mocked hiera data for windows
+shared_context :windows_hiera do
+    # example only,
+    let(:hiera_data) do
+        {:some_key => "some_value" }
+    end
+end
+
+# you cannot use this in addition to any of the hiera_data contexts above
+shared_context :real_hiera_data do
+    let(:hiera_config) do
+       hiera_config_file
+    end
+end
